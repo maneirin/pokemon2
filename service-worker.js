1fa5afd905ec9a8c840fd49a9fd42ca13fd445e3
Thiago <thaigor.luz79@gmail.com>
2026-05-28 18:05:06 -0300

    Updates

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "pokemon-app-cache-v1";
+
+const urlsToCache = [
+  "/"
+];
+
+self.addEventListener("install", (event) => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+
+      .then((cache) => {
+
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+      .then((response) => {
+
+        return response || fetch(event.request);
+      })
+  );
+});
